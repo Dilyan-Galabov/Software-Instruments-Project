c8be5070b780c00e93c9637e7736f7e305345eb7
Dilyan Galabov <galabov@media-it-services.de>
2019-05-27 15:34:56 +0300

    Dependency for project 'C'.

--- A/D/src/main/java/org/D/App.java
+++ A/D/src/main/java/org/D/App.java
@@ -1,13 +1,17 @@
 package org.D;
 
+import org.B.Animal;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // can be accesed because of dependency of C(which depends on B)
+        Animal animal = new Animal();
+        animal.animalSound();
     }
 }
